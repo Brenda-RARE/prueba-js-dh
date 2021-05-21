88ac8cafb3a3d7f49d65d0a47966bc8a9566300a
Brenda-RARE <brendacarmenrare@hotmail.com>
2021-05-20 22:30:18 -0500

    prueba correccion

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-/*
-    FORMATO DE CADA TAREA
-        - titulo: string
-        - status: string (
-           * terminada
-           * pendiente
-         )
-*/
-
+const tareas= [
+    {
+        titulo: 'investigacion',
+        status:'pendiente'
+    },
+    {
+        titulo: 'investigacion',
+        status:'pendiente'
+    }
+];
+const fs = require("fs");
 exports.leerTareas = function () {
-    /*
-        En esta función debes leer y retornar las tareas registradas.
-    */
+    let texto = fs.readFileSync("./tareas.json");
+    return JSON.parse(texto);
 }
-
 exports.agregarTarea = function (tarea) {
-    /*
-        Registra y guarda una nueva tarea.
-    */
+    let tareas = exports.leerTareas();
+    tareas.push(tarea);
+    let formato = JSON.stringify(tareas);
+    fs.writeFileSync("./tareas.json", formato);
 }
-
-exports.filtrarTareasPorEstado = function (estado) {
-    /*
-        En esta función debes de leer las tareas y retornar las que tengan el estado que se
-        manda en el parametro.
-    */
+exports.filtrarTareasPorEstado = function (status) {
+    let tareas = exports.leerTareas();
+    return tareas.filter((tarea) => tarea.status === status);
 }
